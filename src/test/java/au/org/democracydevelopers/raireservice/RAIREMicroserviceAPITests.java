7a6c4831efe6a7eaaab7dc770273328492d22c59
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2023-12-12 18:12:23 +1100

    Added SpringBoot tests. So far these are pretty trivial - they test that certain kinds of bad requests get sensible bad error messages, and that a trivial valid request gets a solution in response.
    Obviously this is a stepping stone to real tests that look at the output from examples.

---
+++ src/test/java/au/org/democracydevelopers/raireservice/RAIREMicroserviceAPITests.java
@@ -0,0 +1,94 @@
+package au.org.democracydevelopers.raireservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/*
+ * Tests for Springboot functioning. This class automatically fires up the RAIRE Microservice on a random port, then runs
+ * a series of (at this stage) very basic tests.
+ * Note that you have to run the *whole class*. Individual tests do not work separately because they don't initiate the
+ * microservice on their own.
+ */
+
+    @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+    public class RAIREMicroserviceAPITests {
+
+        private final static String auditEndpoint = "/cvr/audit";
+
+        @LocalServerPort
+        private int port;
+
+
+        @Autowired
+        private TestRestTemplate restTemplate;
+
+        @Test
+        void contextLoads() {
+        }
+
+        /*
+         * This is really just a test that the testing is working.
+         * There's no mapping for the plain localhost response, so when the microservice is running it just returns
+         * a default error. We check for 404 because that appears in the default error text.
+         */
+        @Test
+        public void testErrorForNonFunctioningEndpoint() {
+            assertTrue((restTemplate.getForObject("http://localhost:" + port + "/",
+                    String.class)).contains("404"));
+        }
+
+        /*
+         * Check that calling the right endpoint with no header and no data produces a sensible error message.
+         */
+        @Test
+        public void testMethodNotAllowed() {
+            assertTrue((restTemplate.getForObject("http://localhost:" + port + auditEndpoint,
+                    String.class)).contains("405"));
+            assertTrue((restTemplate.getForObject("http://localhost:" + port + auditEndpoint,
+                    String.class)).contains("Method Not Allowed"));
+        }
+
+        /*
+         * The right endpoint, with correct headers but no data, should produce "Bad Request".
+         */
+        @Test
+        public void testBadRequest() {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            String url = "http://localhost:" + port + auditEndpoint;
+
+            HttpEntity<String> request = new HttpEntity<>("", headers);
+            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+
+            assertTrue(Objects.requireNonNull(response.getBody()).contains("400"));
+            assertTrue(response.getBody().contains("Bad Request"));
+        }
+
+        /*
+         * At this stage, we are only testing that we get a solution.
+         */
+        @Test
+        public void testTrivialExample() {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            String url = "http://localhost:" +port + auditEndpoint;
+
+            HttpEntity<String> request = new HttpEntity<>("{\"contestName\": \"TrivialExample1\", " +
+                    "\"totalAuditableBallots\": 15, " +
+                    "\"timeProvisionForResult\": 10, " +
+                    " \"candidates\": [\"Alice\",\"Bob\"], " +
+                    "\"votes\":[[\"Alice\",\"Bob\"],[\"Alice\",\"Bob\"]]" +
+                    "}", headers);
+            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class );
+
+            assertTrue(Objects.requireNonNull(response.getBody()).contains("solution"));
+        }
+    }
