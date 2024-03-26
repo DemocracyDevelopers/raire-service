8247c124578d6058776abae2619ce8666f78c603
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2024-03-26 21:27:36 +1100

    Changed ConcreteSTV to raire-service.

--- src/main/java/au/org/democracydevelopers/raireservice/RaireJavaApplication.java
+++ src/main/java/au/org/democracydevelopers/raireservice/RaireJavaApplication.java
@@ -6,7 +6,7 @@ the raire assertion generation engine (https://github.com/DemocracyDevelopers/ra
 This file is part of raire-service.
 raire-service is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 raire-service is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-You should have received a copy of the GNU Affero General Public License along with ConcreteSTV. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License along with raire-service. If not, see <https://www.gnu.org/licenses/>.
 */
 
 package au.org.democracydevelopers.raireservice;
