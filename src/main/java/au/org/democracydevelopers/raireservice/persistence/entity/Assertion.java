f469e0dc65b5058fa6f7959a186290e7c5c27c5e
michelleblom <michelleblom@users.noreply.github.com>
2024-04-08 11:45:30 +1000

    Fleshed out non-nullable fields in Assertion class as per table design.

--- src/main/java/au/org/democracydevelopers/raireservice/persistence/entity/Assertion.java
+++ src/main/java/au/org/democracydevelopers/raireservice/persistence/entity/Assertion.java
@@ -20,6 +20,7 @@ raire-service. If not, see <https://www.gnu.org/licenses/>.
 
 package au.org.democracydevelopers.raireservice.persistence.entity;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import jakarta.persistence.*;
@@ -39,19 +40,19 @@ public abstract class Assertion {
   private Long id;
 
   /**
-   * Name of contest for which this Assertion was generated.
+   * Name of the contest for which this Assertion was generated.
    */
   @Column(name = "contest_name", nullable = false)
   protected String contestName;
 
   /**
-   * Winner of the assertion (a candidate in the contest).
+   * Winner of the Assertion (a candidate in the contest).
    */
   @Column(name = "winner", nullable = false)
   protected String winner;
 
   /**
-   * Loser of the assertion (a candidate in the contest).
+   * Loser of the Assertion (a candidate in the contest).
    */
   @Column(name = "loser", nullable = false)
   protected String loser;
@@ -63,12 +64,69 @@ public abstract class Assertion {
   protected int margin;
 
   /**
-   * List of candidates that the assertion assumes are `continuing' in the
-   * assertion's context.
+   * List of candidates that the Assertion assumes are 'continuing' in the Assertion's context.
    */
   @ElementCollection(fetch = FetchType.EAGER)
   @CollectionTable(name = "assertion_context", joinColumns = @JoinColumn(name = "id"))
   protected List<String> assumedContinuing = new ArrayList<>();
 
+  /**
+   * Diluted margin for the Assertion. This is equal to the assertion margin divided by the
+   * number of ballots in the relevant auditing universe.
+   */
+  @Column(name = "diluted_margin", nullable = false)
+  protected double dilutedMargin = 0;
 
+  /**
+   * Assertion difficulty, as estimated by raire-java. (Note that raire-java has multiple ways
+   * of estimating difficulty, and that these measurements are not necessarily in terms of numbers
+   * of ballots. For example, one method may be: difficulty =  1 / assertion margin).
+   */
+  @Column(name = "difficulty", nullable = false)
+  protected double difficulty = 0;
+
+  /**
+   * The expected number of samples to audit overall for the Assertion, assuming overstatements
+   * continue at the current rate experienced in the audit.
+   */
+  @Column(nullable = false)
+  private Integer my_estimated_samples_to_audit = 0;
+
+  /**
+   * The two-vote understatements recorded against the Assertion.
+   */
+  @Column(nullable = false)
+  protected Integer my_two_vote_under_count = 0;
+
+  /**
+   * The one-vote understatements recorded against the Assertion.
+   */
+  @Column(nullable = false)
+  protected Integer my_one_vote_under_count = 0;
+
+  /**
+   * The one-vote overstatements recorded against the Assertion.
+   */
+  @Column(nullable = false)
+  protected Integer my_one_vote_over_count = 0;
+
+  /**
+   * The two-vote overstatements recorded against the Assertion.
+   */
+  @Column(nullable = false)
+  protected Integer my_two_vote_over_count = 0;
+
+  /**
+   * Discrepancies recorded against the Assertion that are neither understatements nor
+   * overstatements.
+   */
+  @Column(nullable = false)
+  protected Integer my_other_count = 0;
+
+  /**
+   * Current risk measurement recorded against the Assertion. It is initialized to 1, as prior
+   * to an audit starting, and without additional information, we assume maximum risk.
+   */
+  @Column(nullable = false)
+  protected BigDecimal my_current_risk = BigDecimal.valueOf(1);
 }
