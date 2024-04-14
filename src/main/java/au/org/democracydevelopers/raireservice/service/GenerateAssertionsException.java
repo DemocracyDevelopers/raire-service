312b2ccf366781e9bf590e321be278a61063f6d5
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2024-04-14 20:56:38 +1000

    Conversion of RaireErrors.

--- src/main/java/au/org/democracydevelopers/raireservice/service/GenerateAssertionsException.java
+++ src/main/java/au/org/democracydevelopers/raireservice/service/GenerateAssertionsException.java
@@ -20,6 +20,21 @@ raire-service. If not, see <https://www.gnu.org/licenses/>.
 
 package au.org.democracydevelopers.raireservice.service;
 
+import au.org.democracydevelopers.raire.RaireError;
+import au.org.democracydevelopers.raire.RaireError.CouldNotRuleOut;
+import au.org.democracydevelopers.raire.RaireError.InternalErrorDidntRuleOutLoser;
+import au.org.democracydevelopers.raire.RaireError.InternalErrorRuledOutWinner;
+import au.org.democracydevelopers.raire.RaireError.InternalErrorTrimming;
+import au.org.democracydevelopers.raire.RaireError.InvalidCandidateNumber;
+import au.org.democracydevelopers.raire.RaireError.InvalidNumberOfCandidates;
+import au.org.democracydevelopers.raire.RaireError.InvalidTimeout;
+import au.org.democracydevelopers.raire.RaireError.TiedWinners;
+import au.org.democracydevelopers.raire.RaireError.TimeoutCheckingWinner;
+import au.org.democracydevelopers.raire.RaireError.TimeoutFindingAssertions;
+import au.org.democracydevelopers.raire.RaireError.TimeoutTrimmingAssertions;
+import au.org.democracydevelopers.raire.RaireError.WrongWinner;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
 /**
  * Exception indicating that assertion generation failed.
  * These are mostly direct transcriptions of raire-java exceptions, for things such as tied winners
@@ -27,14 +42,100 @@ package au.org.democracydevelopers.raireservice.service;
  */
 public class GenerateAssertionsException extends Exception {
 
-  public int errorCode;
-  public GenerateAssertionsException(String s, int errorCode) {
-    super(s);
-    this.errorCode = errorCode;
+  private static ObjectWriter objectWriter;
+  public RaireErrorCodes errorCode;
+
+  /**
+   * Main constructor, for translating a RaireError into a GenerateAssertionsException.
+   * @param message a human-readable message for the exception.
+   * @param error the RaireError to be expressed as a GenerateAssertionsException.
+   */
+  public GenerateAssertionsException(String message, RaireError error) {
+    super(message);
+    switch (error) {
+      case TiedWinners e -> this.errorCode = RaireErrorCodes.TIED_WINNERS;
+      case TimeoutFindingAssertions e -> this.errorCode = RaireErrorCodes.TIMEOUT_FINDING_ASSERTIONS;
+      case TimeoutTrimmingAssertions e -> this.errorCode = RaireErrorCodes.TIMEOUT_TRIMMING_ASSERTIONS;
+      case TimeoutCheckingWinner e -> this.errorCode = RaireErrorCodes.TIMEOUT_CHECKING_WINNER;
+      case CouldNotRuleOut e -> this.errorCode = RaireErrorCodes.COULD_NOT_RULE_OUT_ALTERNATIVE;
+
+      // TODO: I think this is what we get if the candidate list entered in the request has the
+      // right number but wrong names vs the database. It's therefore not (really) an internal error
+      // - it's a colorado-rla error.
+      case InvalidCandidateNumber e -> this.errorCode = RaireErrorCodes.WRONG_CANDIDATE_NAMES;
+
+      // Internal coding errors.
+      case InvalidTimeout e -> this.errorCode = RaireErrorCodes.INTERNAL_ERROR ;
+      case InternalErrorDidntRuleOutLoser e -> this.errorCode = RaireErrorCodes.INTERNAL_ERROR ;
+      case InternalErrorRuledOutWinner e -> this.errorCode = RaireErrorCodes.INTERNAL_ERROR ;
+      case InternalErrorTrimming e -> this.errorCode = RaireErrorCodes.INTERNAL_ERROR ;
+      case InvalidNumberOfCandidates e -> this.errorCode = RaireErrorCodes.INTERNAL_ERROR ;
+      case WrongWinner e -> this.errorCode = RaireErrorCodes.INTERNAL_ERROR ;
+
+      // We shouldn't have generic Raire errors.
+      case RaireError e -> throw new IllegalStateException("Unexpected value: " + error);
+    }
   }
 
-  public enum errorcode {
+  /**
+   * Message-only constructor, for the case where something went so weirdly wrong we could not even
+   * get an intelligible RaireError.
+   * @param message a human-readable message for the exception.
+   */
+  public GenerateAssertionsException(String message) {
+    super(message);
+    this.errorCode = RaireErrorCodes.INTERNAL_ERROR;
+  }
+
+  public enum RaireErrorCodes {
+
+    // Errors that the user can do something about.
+
+    /**
+     * Tied winners - the contest is a tie and therefore cannot be audited.
+     */
     TIED_WINNERS,
-    NO_SUCH_CONTEST
+
+    /**
+     * Time out checking winners - can happen if the contest is tied, or if it is complicated and
+     * can't be distinguished from a tie.
+     */
+    TIMEOUT_CHECKING_WINNER,
+
+    /**
+     * Raire timed out trying to find assertions. It may succeed
+     * if given more time.
+     */
+    TIMEOUT_FINDING_ASSERTIONS,
+
+    /**
+     * Raire timed out trimming assertions. The assertions have been
+     * generated, but the audit could be more efficient if the trimming step
+     * is re-run with more time allowed.
+     */
+    TIMEOUT_TRIMMING_ASSERTIONS,
+
+    /**
+     * RAIRE couldn't rule out some alternative winner.
+     */
+    COULD_NOT_RULE_OUT_ALTERNATIVE,
+
+    /**
+     * The list of candidate names in the request didn't match the database.
+     * TODO check that this does indeed reflect how we get this error.
+     */
+    WRONG_CANDIDATE_NAMES,
+
+    // Internal errors (that the user can do nothing about)
+
+    /**
+     * A catch-all for various kinds of raire errors that indicate a programming error:
+     * invalid input errors such as InvalidNumberOfCandidates, InvalidTimeout, InvalidCandidateNumber
+     * - these should all be caught before being sent to raire.
+     * Also database errors.
+     * These are errors that the user can't do anything about.
+     */
+    INTERNAL_ERROR,
+
   }
 }
