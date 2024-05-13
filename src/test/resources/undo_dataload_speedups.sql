2007d303f77addbd46dc60c5f73650bbc3a2a8b1
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2024-05-14 09:40:51 +1000

    Remove un-logging.

--- src/test/resources/undo_dataload_speedups.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE public.cast_vote_record SET LOGGED;
-ALTER TABLE public.cvr_contest_info SET LOGGED;
-ALTER TABLE public.cvr_audit_info SET LOGGED;
-ALTER TABLE public.contest_comparison_audit_disagreement SET LOGGED;
-ALTER TABLE public.contest_comparison_audit_discrepancy SET LOGGED;
-ALTER TABLE public.county_contest_comparison_audit_disagreement SET LOGGED;
-ALTER TABLE public.county_contest_comparison_audit_discrepancy SET LOGGED;
-
